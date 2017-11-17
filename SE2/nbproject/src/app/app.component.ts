1a397ff2349c573b3efcc4c01c25206ea81189de
Nasdon33 <carlo.1994@live.it>
2017-11-17 14:35:22 +0100

    Aggiungo base per query DBpedia

--- SE2/nbproject/src/app/app.component.ts
+++ SE2/nbproject/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, RequestOptionsArgs, Headers, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'my-app',
@@ -21,5 +23,33 @@ export class AppComponent  {
       setTimeout(3000); // Change image every 2 seconds
   }
 
+  // Da qua in giù funzione DBpedia
+
+  private sparkqlData = null;
+  constructor(private http: Http) { }
+
+  sparkql() {
+    let headers: Headers = new Headers({
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    });
+
+    let params = new URLSearchParams();
+    params.append('query', 'SELECT * WHERE { <http://dbpedia.org/resource/Awolnation> ?pref ?obj } LIMIT 10');
+    params.append('format', 'json');
+
+    let options: RequestOptionsArgs = {
+      headers: headers,
+      params: URLSearchParams
+    };
+
+    this.http.get('http://dbpedia.org/sparql', options) // 1
+      .map(response => response.json())
+      .subscribe(data => {
+          console.log(data);
+          this.sparkqlData = data; // 3
+       });
+     console.log(this.sparkqlData);  // 2
+  }
 }
 

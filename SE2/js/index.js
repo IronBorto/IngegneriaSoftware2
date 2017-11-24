d3798dd96df3e49a5aaa0d535914cc7638904514
ZappO <luca.zappini-1@studenti.unitn.it>
2017-11-24 14:17:08 +0100

    AggiuntaModuloEsistemazioneGooglekSearch

--- SE2/js/index.js
+++ SE2/js/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var db = require('./dbPediaSearch');
+var gsearch = require('./google-search');
 //inspect variables
 var util = require('util');
 
@@ -8,13 +10,13 @@ var app = express();
 
 //handle get req on /page1
 app.get('/page1', function (req, res) {
-	dbpedia(req.query.value);
+	db.dbpedia(req.query.value);
 });
 
 
 //handle get req on /page2
 app.get('/page2', function (req, res) {
-    res.send('Page2');
+    gsearch.gsearchv2();
 });
 
 
@@ -29,31 +31,6 @@ app.post('/page2', function (req, res) {
     res.send('POST Page2');
     
 });
-
-function dbpedia(param){
-	var SparqlClient = require('sparql-client');
-	var util = require('util');
-	var endpoint = 'http://dbpedia.org/sparql';
-	var query = /*"prefix dbpedia: <http://dbpedia.org/resource/>"+*/
-	"prefix dbpedia-owl: <http://dbpedia.org/ontology/>"+
-	
-	"select ?abstract ?thumbnail where { "+
-	"  <http://dbpedia.org/resource/"+ param +"> dbpedia-owl:abstract ?abstract ;"+
-							   "dbpedia-owl:thumbnail ?thumbnail ."+
-	"  filter(langMatches(lang(?abstract),\"en\"))"+
-	"}";
-	var client = new SparqlClient(endpoint);
-	console.log("Query to " + endpoint);
-	console.log("Query: " + query);
-	client.query(query)
-	  .execute(function(error, results) {
-	  process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
-	});
-}
-
-
-
-
 
 //listen in a specific port
 app.listen((process.env.PORT || 80));

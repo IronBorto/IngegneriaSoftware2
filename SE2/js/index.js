f63f04719ee3544fb7ab0c37719048ea6a34b6ce
ZappO <luca.zappini-1@studenti.unitn.it>
2017-11-29 12:03:46 +0100

    Aggiunta caricamento immagine

--- SE2/js/index.js
+++ SE2/js/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('./dbPediaSearch');
 var gsearch = require('./google-search');
-//inspect variables
 var util = require('util');
+var fs = require('fs');
+var util = require('util');
+var mime = require('mime');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 //instantiate express
 var app = express();
@@ -31,6 +35,15 @@ app.post('/page2', function (req, res) {
     res.send('POST Page2');
     
 });
+
+
+app.post('/upload', upload.single('image'), function(req, res, next) {
+      console.log('Sono in upload ' + req.file.path);
+        fs.unlinkSync(req.file.path);
+        console.log('fine e cancellazione')
+    });
+
+
 
 //listen in a specific port
 app.listen((process.env.PORT || 80));
